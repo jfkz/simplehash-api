4e1d43a2c51e2f05cceb48a8fc437c8a76941758
mike <jfkz@users.noreply.github.com>
2025-01-29 16:33:14 +0000

    feat: monad testnet

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -3,6 +3,6 @@ export type Chain = 'ethereum' | 'polygon' | 'solana' | 'optimism' | 'arbitrum' 
   | 'zksync-era' | 'zora' | 'ethereum-goerli' | 'ethereum-rinkeby' | 'ethereum-sepolia' | 'solana-devnet' | 'solana-testnet'
   | 'polygon-mumbai' | 'arbitrum-goerli' | 'avalanche-fuji' | 'base-goerli' | 'bsc-testnet' | 'frame-testnet'
   | 'godwoken-testnet' | 'linea-testnet' | 'manta-testnet' | 'optimism-goerli' | 'palm-testnet' | 'palm-testnet-edge'
-  | 'polygon-zkevm-testnet' | 'scroll-testnet' | 'scroll-sepolia' | 'zksync-era-testnet' | 'zora-testnet';
+  | 'polygon-zkevm-testnet' | 'scroll-testnet' | 'scroll-sepolia' | 'zksync-era-testnet' | 'zora-testnet' | 'monad-testnet';
 export type Order = 'timestamp_desc' | 'timestamp_asc';
 export type Marketplace = 'opensea' | 'quixotic' | 'stratos' | 'trove';
